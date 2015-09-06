89f3863f6877d8e1f57ab266c81e5e99f99bf601
pg20 <pramitigoel20@gmail.com>
2015-09-06 14:38:41 +0530

    layout-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+root             'staticpages#home'
+  get 'help'    => 'staticpages#help'
+  get 'about'   => 'staticpages#about'
+  get 'contact' => 'staticpages#contact'
+  get 'signup'  => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
